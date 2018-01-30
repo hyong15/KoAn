8e31c9ecafe9e299ea9397bcfe9dc35489eab346
HYONG15 <136150782@qq.com>
2018-01-30 23:45:33 +0800

    扩展方法

--- feature/src/main/java/com/example/hyong/koan/feature/KoActivity.kt
+++ feature/src/main/java/com/example/hyong/koan/feature/KoActivity.kt
@@ -1,15 +1,24 @@
 package com.example.hyong.koan.feature
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 
 class KoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ko)
-        nullExercise()
+        nullExercise()//空练习
+        extraExercise()//扩展方法
+    }
+
+    private fun extraExercise() {
+        fun Fragment.toast(message:CharSequence, duration:Int = Toast.LENGTH_LONG){
+            Toast.makeText(activity, message, duration).show()
+        }
     }
 
     private fun nullExercise() {
